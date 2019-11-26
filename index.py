ba8a382a74eec39484c521ecb2cdfea250cb5360
Magin-CC <573746388@qq.com>
2019-11-26 19:11:12 +0800

    feat: add comment

--- index.py
+++ index.py
@@ -55,22 +55,22 @@ class Wlxy:
                     time.sleep(1)
             self.listNextPage()
         time.sleep(1000000)
-
+    # 登陆后的加载动作
     def waitHelper(self, browser):
         return browser.title == '首页'
-
+    # 课程列表的加载动作
     def waitHelper2(self, browser):
         _cond1 = len(browser.find_elements_by_class_name('course-box')) > 0
         _cond2 = len(browser.find_elements_by_class_name(
             'ant-spin-spinning')) == 0
         return _cond1 and _cond2
-
+    # 课程详情的加载动作
     def waitHelper3(self, browser):
         _cond1 = len(browser.find_elements_by_class_name('color-data')) > 0
         _cond2 = len(browser.find_elements_by_class_name(
             'ant-spin-spinning')) == 0
         return _cond1 and _cond2
-
+    # tab到课程详情页
     def jump2Window2(self):
         newHandles = self.browser.window_handles
         # 跳转到新窗口
@@ -80,7 +80,7 @@ class Wlxy:
                     # 切换到新打开的窗口B
                 self.browser.switch_to.window(newhandle)
                 self.window2Handle = newhandle
-
+    # tab到播放页
     def jump2Window3(self):
         newHandles = self.browser.window_handles
         # 跳转到新窗口
@@ -91,14 +91,15 @@ class Wlxy:
                 self.browser.switch_to.window(newhandle)
                 self.window3Handle = newhandle
 
+    # 关闭播放页
     def closeWindow3(self):
         self.browser.close()
         self.browser.switch_to.window(self.window2Handle)
-
+    # 关闭课程详情页
     def closeWindow2(self):
         self.browser.close()
         self.browser.switch_to.window(self.window1Handle)
-
+    # 登录动作
     def login(self):
         self.browser.find_elements_by_class_name(
             'ant-input')[0].send_keys(self.username)
@@ -119,14 +120,14 @@ class Wlxy:
             'input-box')[0].send_keys(captcha_str)
         self.browser.find_elements_by_class_name(
             'login-form-button')[0].click()
-
+    # 从首页跳转到列表页
     def jump2List(self):
         self.browser.find_elements_by_class_name('ng-tns-c8-0')[5].click()
-
+    # 从列表页跳转到详情页
     def openVideoDetail(self, index):
         self.browser.find_elements_by_class_name(
             'course-box')[index - 1].click()
-
+    # 从详情页到播放页
     def startVideo(self):
         score = float(self.browser.find_elements_by_class_name(
             'color-data')[2].text)
@@ -138,13 +139,13 @@ class Wlxy:
         self.addScoreAndPrint(score)
         self.browser.find_element_by_class_name('info-btn').click()
         return True
-
+    # 加分动作
     def addScoreAndPrint(self, score):
         self.score += score
         print('本次学习已得分：' + str(self.score))
         if(self.score > 70):
             raise Exception('70分都不止了，够高了，别刷了大哥！')
-
+    # 核心代码         
     def execScript(self):
         self.browser.execute_script('XMLHttpRequest.prototype._send = XMLHttpRequest.prototype.send;' +
                                     'XMLHttpRequest.prototype.send = function(data) {' +
@@ -161,12 +162,12 @@ class Wlxy:
                                     '}' +
                                     '  return true;' +
                                     '};')
-
+    # 保存并退出动作
     def saveAndQuit(self):
         btnDom = self.browser.find_elements_by_class_name('ant-btn-primary')
         if len(btnDom) > 0:
             btnDom[0].click()
-
+    # 列表页翻页动作
     def listNextPage(self):
         self.browser.execute_script(
             "$('.ant-pagination-item-active').next().click()")
