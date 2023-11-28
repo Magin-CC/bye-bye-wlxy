9ae578cceade210ca004d8e98ddf8169b323150a
jiaxu <wangchaomagin@gmail.com>
2023-11-28 10:18:20 +0800

    fix: 2023update

--- index.py
+++ index.py
@@ -14,27 +14,33 @@ import math
 
 class Wlxy:
     def __del__(self):
-        self.browser.quit()
-        self.b2.quit()
+        print('退出类')
 
-    def __init__(self, username, pwd):
+    def __init__(self, username, pwd, sc):
         self.startUrl = 'https://wlxy.8531.cn/learner/login'
         self.score = 0
+        self.sc = sc
         self.username = username
         self.pwd = pwd
         option = webdriver.ChromeOptions()
         option.add_argument("headless")
-        # option.add_experimental_option(
-        #     "prefs", {"profile.managed_default_content_settings.images": 2})
+        option.add_experimental_option(
+            "prefs", {"profile.managed_default_content_settings.images": 2})
         self.browser = webdriver.Chrome(options=option)
         self.browser.get(self.startUrl)
         self.window1Handle = self.browser.current_window_handle
         self.login()
         WebDriverWait(self.browser, 20, 0.5).until(self.waitHelper)
-        self.jump2List()
+        self.browser.get('https://wlxy.8531.cn/learner/opencourse')
+        # self.jump2List()
+        time.sleep(5)
+        # self.jump2NotMust()
+        self.window1Handle = self.browser.current_window_handle
+
+        time.sleep(5)
         for i in range(80):
             WebDriverWait(self.browser, 20, 0.5).until(self.waitHelper2)
-            for i in range(23):
+            for i in range(10):
                 self.openVideoDetail(i + 1)
                 self.jump2Window2()
                 WebDriverWait(self.browser, 20, 0.5).until(self.waitHelper3)
@@ -56,21 +62,25 @@ class Wlxy:
             self.listNextPage()
         time.sleep(1000000)
     # 登陆后的加载动作
+
     def waitHelper(self, browser):
-        return browser.title == '首页'
+        return browser.title == '首页' or browser.title == '浙报集团网络学院'
     # 课程列表的加载动作
+
     def waitHelper2(self, browser):
-        _cond1 = len(browser.find_elements_by_class_name('course-box')) > 0
-        _cond2 = len(browser.find_elements_by_class_name(
+        _cond1 = len(browser.find_elements(By.CLASS_NAME, 'course-box')) > 0 or len(browser.find_elements(By.CLASS_NAME, 'single-course')) > 0
+        _cond2 = len(browser.find_elements(By.CLASS_NAME, 
             'ant-spin-spinning')) == 0
         return _cond1 and _cond2
     # 课程详情的加载动作
+
     def waitHelper3(self, browser):
-        _cond1 = len(browser.find_elements_by_class_name('color-data')) > 0
-        _cond2 = len(browser.find_elements_by_class_name(
+        _cond1 = len(browser.find_elements(By.CLASS_NAME, 'color-data')) > 0
+        _cond2 = len(browser.find_elements(By.CLASS_NAME, 
             'ant-spin-spinning')) == 0
         return _cond1 and _cond2
     # tab到课程详情页
+
     def jump2Window2(self):
         newHandles = self.browser.window_handles
         # 跳转到新窗口
@@ -81,6 +91,7 @@ class Wlxy:
                 self.browser.switch_to.window(newhandle)
                 self.window2Handle = newhandle
     # tab到播放页
+
     def jump2Window3(self):
         newHandles = self.browser.window_handles
         # 跳转到新窗口
@@ -96,56 +107,64 @@ class Wlxy:
         self.browser.close()
         self.browser.switch_to.window(self.window2Handle)
     # 关闭课程详情页
+
     def closeWindow2(self):
         self.browser.close()
         self.browser.switch_to.window(self.window1Handle)
     # 登录动作
-    def login(self):
-        self.browser.find_elements_by_class_name(
-            'ant-input')[0].send_keys(self.username)
-        self.browser.find_elements_by_class_name(
-            'ant-input')[1].send_keys(self.pwd)
 
-        captcha = self.browser.find_elements_by_class_name(
-            'img-box')[1].get_attribute('src')
-        self.b2 = webdriver.Chrome()
-        self.b2.set_window_size(300, 100)
-        self.b2.get(captcha)
-        captcha_str = input('请输入显示的验证码: ')
-        sessionId = self.b2.get_cookie('SESSION')
-        self.browser.add_cookie(
-            {'name': 'SESSION', 'value': sessionId.get('value')})
-        self.b2.close()
-        self.browser.find_elements_by_class_name(
-            'input-box')[0].send_keys(captcha_str)
-        self.browser.find_elements_by_class_name(
-            'login-form-button')[0].click()
+    def login(self):
+        self.browser.switch_to.frame('idaasLoginPlugin')
+        time.sleep(10)
+        loginbox = self.browser.find_elements(By.CLASS_NAME, 'login-box')[2]
+        loginbox.find_elements(By.CLASS_NAME, 
+            'ivu-input')[0].send_keys(self.username)
+        loginbox.find_elements(By.CLASS_NAME, 
+            'ivu-input')[1].send_keys(self.pwd)
+
+        loginbox.find_elements(By.CLASS_NAME, 
+            'ivu-btn')[1].click()
     # 从首页跳转到列表页
+
     def jump2List(self):
-        self.browser.find_elements_by_class_name('ng-tns-c8-0')[5].click()
+        eles = self.browser.find_elements(By.CLASS_NAME, 'ng-tns-c2-0')
+        print(len(eles));
+        eles[4].click()
     # 从列表页跳转到详情页
+
+    def jump2Must(self):
+        # print(self.browser.find_element_by_xpath('span[text()="必修"]'))
+        self.browser.find_element(By.XPATH, '//span[text()="必修"]').click()
+
+    def jump2NotMust(self):
+        self.browser.find_element(By.XPATH, '//span[text()="选修"]').click()
+
     def openVideoDetail(self, index):
-        self.browser.find_elements_by_class_name(
-            'course-box')[index - 1].click()
+        self.browser.find_elements(By.CLASS_NAME, 
+            'course-info')[index - 1].click()
     # 从详情页到播放页
+
     def startVideo(self):
-        score = float(self.browser.find_elements_by_class_name(
+        score = float(self.browser.find_elements(By.CLASS_NAME, 
             'color-data')[2].text)
         print('进入课程: ' + self.browser.title + '(' + str(score) + '分)')
-        if len(self.browser.find_elements_by_class_name('study-type')) > 1:
-            if self.browser.find_elements_by_class_name('study-type')[1].text == '已完成':
+        if len(self.browser.find_elements(By.CLASS_NAME, 'study-type')) > 1:
+            if self.browser.find_elements(By.CLASS_NAME, 'study-type')[1].text == '已完成':
                 print('该课程已学过')
                 return False
         self.addScoreAndPrint(score)
-        self.browser.find_element_by_class_name('info-btn').click()
+        self.browser.find_element(By.CLASS_NAME, 'info-btn').click()
         return True
     # 加分动作
+
     def addScoreAndPrint(self, score):
         self.score += score
         print('本次学习已得分：' + str(self.score))
-        if(self.score > 70):
-            raise Exception('70分都不止了，够高了，别刷了大哥！')
-    # 核心代码         
+        if(self.score > 90):
+            print('已成功刷了', self.score, '分')
+            sys.exit(0)
+    # 核心代码
+
     def execScript(self):
         self.browser.execute_script('XMLHttpRequest.prototype._send = XMLHttpRequest.prototype.send;' +
                                     'XMLHttpRequest.prototype.send = function(data) {' +
@@ -163,21 +182,21 @@ class Wlxy:
                                     '  return true;' +
                                     '};')
     # 保存并退出动作
+
     def saveAndQuit(self):
-        btnDom = self.browser.find_elements_by_class_name('ant-btn-primary')
+        btnDom = self.browser.find_elements(By.CLASS_NAME, 'ant-btn-primary')
         if len(btnDom) > 0:
             btnDom[0].click()
     # 列表页翻页动作
+
     def listNextPage(self):
         self.browser.execute_script(
             "$('.ant-pagination-item-active').next().click()")
 
 
 print('\n\n===+++---this program power by Magin---+++===\n\n')
-username = input('输入账号: ')
-pwd = input('输入密码: ')
-try:
-    wlxy = Wlxy(username, pwd)
-except:
-    print(sys.exc_info()[0])
-    time.sleep(1000000)
+# username = input('输入账号: ')
+# pwd = input('输入密码: ')
+# sc = input('输入要刷的分数: ')
+# wlxy = Wlxy(username, pwd, 50)
+
